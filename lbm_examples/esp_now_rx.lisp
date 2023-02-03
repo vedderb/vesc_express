97f987c8d1beb0af800beb6f7f1a1d27ff643cb2
Benjamin Vedder <benjamin@vedder.se>
2023-02-03 21:03:14 +0100

    Updated esp-now example

--- lbm_examples/esp_now_rx.lisp
+++ lbm_examples/esp_now_rx.lisp
@@ -2,21 +2,15 @@
 
 ; Data contains an array with src, dst and content
 (defun proc-data (data)
-    (progn
-        (def src (map (fn (x) (bufget-u8 data x)) (range 0 6)))
-        (def des (map (fn (x) (bufget-u8 data x)) (range 6 12)))
-        
-        ; Content as list
-        (def content (map (fn (x) (bufget-u8 data x)) (range 12 (buflen data))))
-        
-        ; Content as array
-;        (def content (array-create (- (buflen data) 12)))
-;        (bufcpy content 0 data 12 (buflen content))
-
-        (print (list "src:" src "des:" des "content:" content))
-        (free data)
-;        (free content)
-))
+    (let (
+            (src (map (fn (x) (bufget-u8 data x)) (range 0 6)))
+            (des (map (fn (x) (bufget-u8 data x)) (range 6 12)))
+            (content (map (fn (x) (bufget-u8 data x)) (range 12 (buflen data))))
+        )
+        (progn
+            (print (list "src:" src "des:" des "content:" content))
+            (free data)
+)))
         
 (defun event-handler ()
     (loopwhile t
