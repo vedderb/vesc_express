a9e1564139b421646e1dde2e71fb32ceadb08da4
Benjamin Vedder <benjamin@vedder.se>
2023-02-21 15:29:58 +0100

    Moved rgbled-driver from devkit to extensions and made it genrel purpose

--- main/lispif_vesc_extensions.c
+++ main/lispif_vesc_extensions.c
@@ -42,6 +42,8 @@
 #include "esp_now.h"
 #include "esp_crc.h"
 #include "driver/i2c.h"
+#include "driver/rmt_encoder.h"
+#include "driver/rmt_tx.h"
 
 #include <math.h>
 #include <ctype.h>
@@ -1408,6 +1410,224 @@ static lbm_value ext_crc16(lbm_value *args, lbm_uint argn) {
 	return lbm_enc_i(crc16((uint8_t*)array->data, len));
 }
 
+// WS2812-driver using RMT
+
+#define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+
+typedef struct {
+	rmt_encoder_t base;
+	rmt_encoder_t *bytes_encoder;
+	rmt_encoder_t *copy_encoder;
+	int state;
+	rmt_symbol_word_t reset_code;
+} rmt_led_strip_encoder_t;
+
+static rmt_channel_handle_t led_chan = NULL;
+static rmt_encoder_handle_t led_encoder = NULL;
+static uint8_t *led_pixels = NULL;
+static int led_num = -1;
+
+static rmt_transmit_config_t tx_config = {
+		.loop_count = 0, // no transfer loop
+};
+
+static size_t rmt_encode_led_strip(
+		rmt_encoder_t *encoder, rmt_channel_handle_t channel,
+		const void *primary_data, size_t data_size, rmt_encode_state_t *ret_state) {
+	rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
+	rmt_encoder_handle_t bytes_encoder = led_encoder->bytes_encoder;
+	rmt_encoder_handle_t copy_encoder = led_encoder->copy_encoder;
+	rmt_encode_state_t session_state = 0;
+	rmt_encode_state_t state = 0;
+	size_t encoded_symbols = 0;
+	switch (led_encoder->state) {
+	case 0: // send RGB data
+		encoded_symbols += bytes_encoder->encode(bytes_encoder, channel, primary_data, data_size, &session_state);
+		if (session_state & RMT_ENCODING_COMPLETE) {
+			led_encoder->state = 1; // switch to next state when current encoding session finished
+		}
+		if (session_state & RMT_ENCODING_MEM_FULL) {
+			state |= RMT_ENCODING_MEM_FULL;
+			goto out; // yield if there's no free space for encoding artifacts
+		}
+		// fall-through
+		//no break
+	case 1: // send reset code
+		encoded_symbols += copy_encoder->encode(copy_encoder, channel, &led_encoder->reset_code,
+				sizeof(led_encoder->reset_code), &session_state);
+		if (session_state & RMT_ENCODING_COMPLETE) {
+			led_encoder->state = 0; // back to the initial encoding session
+			state |= RMT_ENCODING_COMPLETE;
+		}
+		if (session_state & RMT_ENCODING_MEM_FULL) {
+			state |= RMT_ENCODING_MEM_FULL;
+			goto out; // yield if there's no free space for encoding artifacts
+		}
+	}
+	out:
+	*ret_state = state;
+	return encoded_symbols;
+}
+
+static esp_err_t rmt_del_led_strip_encoder(rmt_encoder_t *encoder) {
+	rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
+	rmt_del_encoder(led_encoder->bytes_encoder);
+	rmt_del_encoder(led_encoder->copy_encoder);
+	free(led_encoder);
+	return ESP_OK;
+}
+
+static esp_err_t rmt_led_strip_encoder_reset(rmt_encoder_t *encoder) {
+	rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
+	rmt_encoder_reset(led_encoder->bytes_encoder);
+	rmt_encoder_reset(led_encoder->copy_encoder);
+	led_encoder->state = 0;
+	return ESP_OK;
+}
+
+esp_err_t rmt_new_led_strip_encoder(rmt_encoder_handle_t *ret_encoder) {
+	rmt_led_strip_encoder_t *led_encoder = NULL;
+	led_encoder = calloc(1, sizeof(rmt_led_strip_encoder_t));
+	led_encoder->base.encode = rmt_encode_led_strip;
+	led_encoder->base.del = rmt_del_led_strip_encoder;
+	led_encoder->base.reset = rmt_led_strip_encoder_reset;
+
+	// different led strip might have its own timing requirements, following parameter is for WS2812
+	rmt_bytes_encoder_config_t bytes_encoder_config = {
+			.bit0 = {
+					.level0 = 1,
+					.duration0 = 0.3 * RMT_LED_STRIP_RESOLUTION_HZ / 1000000, // T0H=0.3us
+					.level1 = 0,
+					.duration1 = 0.9 * RMT_LED_STRIP_RESOLUTION_HZ / 1000000, // T0L=0.9us
+			},
+			.bit1 = {
+					.level0 = 1,
+					.duration0 = 0.9 * RMT_LED_STRIP_RESOLUTION_HZ / 1000000, // T1H=0.9us
+					.level1 = 0,
+					.duration1 = 0.3 * RMT_LED_STRIP_RESOLUTION_HZ / 1000000, // T1L=0.3us
+			},
+			.flags.msb_first = 1 // WS2812 transfer bit order: G7...G0R7...R0B7...B0
+	};
+
+	rmt_new_bytes_encoder(&bytes_encoder_config, &led_encoder->bytes_encoder);
+	rmt_copy_encoder_config_t copy_encoder_config = {};
+	rmt_new_copy_encoder(&copy_encoder_config, &led_encoder->copy_encoder);
+
+	uint32_t reset_ticks = RMT_LED_STRIP_RESOLUTION_HZ / 1000000 * 50 / 2; // reset code duration defaults to 50us
+	led_encoder->reset_code = (rmt_symbol_word_t) {
+		.level0 = 0,
+				.duration0 = reset_ticks,
+				.level1 = 0,
+				.duration1 = reset_ticks,
+	};
+
+	*ret_encoder = &led_encoder->base;
+	return ESP_OK;
+}
+
+static lbm_value ext_rgbled_deinit(lbm_value *args, lbm_uint argn) {
+	(void)args; (void)argn;
+
+	if (led_pixels != NULL) {
+		free(led_pixels);
+		led_pixels = NULL;
+	}
+
+	if (led_chan != NULL) {
+		rmt_disable(led_chan);
+		rmt_del_channel(led_chan);
+		led_chan = NULL;
+	}
+
+	if (led_encoder != NULL) {
+		rmt_del_encoder(led_encoder);
+		led_encoder = NULL;
+	}
+
+	led_num = -1;
+
+	return ENC_SYM_TRUE;
+}
+
+static lbm_value ext_rgbled_init(lbm_value *args, lbm_uint argn) {
+	LBM_CHECK_ARGN_NUMBER(2);
+
+	int pin = lbm_dec_as_i32(args[0]);
+	if (!gpio_is_valid(pin)) {
+		lbm_set_error_reason(pin_invalid_msg);
+		return ENC_SYM_TERROR;
+	}
+
+	int num_leds = lbm_dec_as_u32(args[1]);
+
+	if (num_leds == 0) {
+		lbm_set_error_reason("At least one led must be used");
+		return ENC_SYM_TERROR;
+	}
+
+	ext_rgbled_deinit(0, 0);
+
+	led_pixels = calloc(num_leds, 3);
+
+	if (!led_pixels) {
+		lbm_set_error_reason("Not enough memory");
+		return ENC_SYM_EERROR;
+	}
+
+	led_num = num_leds;
+
+	rmt_tx_channel_config_t tx_chan_config = {
+			.clk_src = RMT_CLK_SRC_DEFAULT, // select source clock
+			.gpio_num = pin,
+			.mem_block_symbols = 64, // increase the block size can make the LED less flickering
+			.resolution_hz = RMT_LED_STRIP_RESOLUTION_HZ,
+			.trans_queue_depth = 4, // set the number of transactions that can be pending in the background
+	};
+	rmt_new_tx_channel(&tx_chan_config, &led_chan);
+
+	rmt_new_led_strip_encoder(&led_encoder);
+	rmt_enable(led_chan);
+
+	return ENC_SYM_TRUE;
+}
+
+static lbm_value ext_rgbled_color(lbm_value *args, lbm_uint argn) {
+	LBM_CHECK_ARGN_NUMBER(2);
+
+	if (led_encoder == NULL || led_chan == NULL || led_pixels == NULL) {
+		lbm_set_error_reason("Please run (rgbled-init pin num-leds) first");
+		if (led_encoder == NULL) {
+			commands_printf_lisp("led_encoder null");
+		}
+
+		if (led_chan == NULL) {
+			commands_printf_lisp("led_chan null");
+		}
+		return ENC_SYM_EERROR;
+	}
+
+	int led = lbm_dec_as_u32(args[0]);
+
+	if (led >= led_num) {
+		lbm_set_error_reason("Invalid LED number");
+		return ENC_SYM_TERROR;
+	}
+
+	uint32_t color = lbm_dec_as_u32(args[1]);
+
+	uint8_t r = (color >> 16) & 0xFF;
+	uint8_t g = (color >> 8) & 0xFF;
+	uint8_t b = color & 0xFF;
+
+	led_pixels[led * 3 + 0] = g;
+	led_pixels[led * 3 + 1] = r;
+	led_pixels[led * 3 + 2] = b;
+
+	rmt_transmit(led_chan, led_encoder, led_pixels, sizeof(led_pixels), &tx_config);
+
+	return ENC_SYM_TRUE;
+}
+
 static lbm_value ext_empty(lbm_value *args, lbm_uint argn) {
 	(void)args;(void)argn;
 	return ENC_SYM_TRUE;
@@ -1496,6 +1716,11 @@ void lispif_load_vesc_extensions(void) {
 	lbm_add_extension("wifi-set-chan", ext_wifi_set_chan);
 	lbm_add_extension("wifi-get-bw", ext_wifi_get_bw);
 	lbm_add_extension("wifi-set-bw", ext_wifi_set_bw);
+
+	// RGBLED
+	lbm_add_extension("rgbled-init", ext_rgbled_init);
+	lbm_add_extension("rgbled-deinit", ext_rgbled_deinit);
+	lbm_add_extension("rgbled-color", ext_rgbled_color);
 
 	// Extension libraries
 	lbm_array_extensions_init();
