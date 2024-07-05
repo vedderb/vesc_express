5ee7d0f820ffaf7f27cbeb52fa24d610b2c82b40
Benjamin Vedder <benjamin@vedder.se>
2024-07-05 17:01:08 +0200

    Fixed indentation

--- main/hwconf/trampa/hw_disp_v1.c
+++ main/hwconf/trampa/hw_disp_v1.c
@@ -15,7 +15,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    */
+ */
 
 #include "hw_disp_v1.h"
 
@@ -42,20 +42,20 @@ static float v_ext = 0.0;
 static float v_btn = 0.0;
 
 #if DISP_HW_VERSION == DISP_V1_3
-	static int pin_3v3 = 4;
-	static int pin_btn = 2;
+static int pin_3v3 = 4;
+static int pin_btn = 2;
 #elif DISP_HW_VERSION == DISP_V1_2
-	static int pin_bl = 5;
-	static int pin_3v3 = 21;
-	static int pin_psw = 2;
-	static int pin_btn = 3;
+static int pin_bl = 5;
+static int pin_3v3 = 21;
+static int pin_psw = 2;
+static int pin_btn = 3;
 #elif DISP_HW_VERSION == DISP_V0
-	static int pin_bl = 3;
-	static int pin_3v3 = 21;
-	static int pin_psw = -1;
-	static int pin_btn = 2;
+static int pin_bl = 3;
+static int pin_3v3 = 21;
+static int pin_psw = -1;
+static int pin_btn = 2;
 #else
-	#error "Incompatible DISP_HW_VERSION"
+#error "Incompatible DISP_HW_VERSION"
 #endif
 
 
@@ -83,112 +83,112 @@ static lbm_value ext_v_btn(lbm_value *args, lbm_uint argn) {
 }
 
 #if DISP_HW_VERSION == DISP_V1_3
-	#define GPIO_EXP_INPUT_REG  0x00
-	#define GPIO_EXP_OUTPUT_REG 0x01
-	#define GPIO_EXP_CONFIG_REG 0x03
+#define GPIO_EXP_INPUT_REG  0x00
+#define GPIO_EXP_OUTPUT_REG 0x01
+#define GPIO_EXP_CONFIG_REG 0x03
 
-	static SemaphoreHandle_t 	i2c_mutex;
+static SemaphoreHandle_t 	i2c_mutex;
 
-	static void i2c_init(void) {
-		i2c_mutex = xSemaphoreCreateMutex();
+static void i2c_init(void) {
+	i2c_mutex = xSemaphoreCreateMutex();
 
-		i2c_config_t conf = {
-				.mode = I2C_MODE_MASTER,
-				.sda_io_num = I2C_SDA,
-				.scl_io_num = I2C_SCL,
-				.sda_pullup_en = GPIO_PULLUP_ENABLE,
-				.scl_pullup_en = GPIO_PULLUP_ENABLE,
-				.master.clk_speed = 100000,
-		};
-		i2c_param_config(0, &conf);
-		i2c_driver_install(0, conf.mode, 0, 0, 0);
-	}
+	i2c_config_t conf = {
+			.mode = I2C_MODE_MASTER,
+			.sda_io_num = I2C_SDA,
+			.scl_io_num = I2C_SCL,
+			.sda_pullup_en = GPIO_PULLUP_ENABLE,
+			.scl_pullup_en = GPIO_PULLUP_ENABLE,
+			.master.clk_speed = 100000,
+	};
+	i2c_param_config(0, &conf);
+	i2c_driver_install(0, conf.mode, 0, 0, 0);
+}
 
-	static esp_err_t i2c_tx_rx(uint8_t addr,
+static esp_err_t i2c_tx_rx(uint8_t addr,
 		const uint8_t* write_buffer, size_t write_size,
 		uint8_t* read_buffer, size_t read_size) {
 
-		xSemaphoreTake(i2c_mutex, portMAX_DELAY);
+	xSemaphoreTake(i2c_mutex, portMAX_DELAY);
 
-		esp_err_t res;
-		if (read_size > 0 && read_buffer != NULL) {
-			if (write_size > 0 && write_buffer != NULL) {
-				res = i2c_master_write_read_device(0, addr, write_buffer, write_size, read_buffer, read_size, 2000);
+	esp_err_t res;
+	if (read_size > 0 && read_buffer != NULL) {
+		if (write_size > 0 && write_buffer != NULL) {
+			res = i2c_master_write_read_device(0, addr, write_buffer, write_size, read_buffer, read_size, 2000);
+		} else {
+			res = i2c_master_read_from_device(0, addr, read_buffer, read_size, 2000);
+		}
+	} else {
+		res = i2c_master_write_to_device(0, addr, write_buffer, write_size, 2000);
+	}
+
+	xSemaphoreGive(i2c_mutex);
+
+	return res;
+}
+
+static int i2c_read_reg(uint8_t addr, uint8_t reg) {
+	uint8_t tx_buf[1] = {reg};
+	uint8_t rx_buf[1];
+	esp_err_t e = i2c_tx_rx(addr, tx_buf, 1, rx_buf, 1);
+	if (e == ESP_OK) {
+		return rx_buf[0];
+	} else {
+		return -1;
+	}
+}
+
+static esp_err_t i2c_write_reg(uint8_t addr, uint8_t reg, uint8_t val) {
+	uint8_t tx_buf[2] = {reg, val};
+	return i2c_tx_rx(addr, tx_buf, 2, 0, 0);
+}
+
+void init_gpio_expander(void) {
+	i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_CONFIG_REG, 0x07); // Ports 0-2 Input
+	i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG, 0x00); // All Output Disable
+}
+
+static lbm_value ext_set_io(lbm_value *args, lbm_uint argn) {
+	if (argn == 2 && lbm_is_number(args[0]) && lbm_is_number(args[1])) {
+		lbm_uint i = lbm_dec_as_u32(args[0]);
+		lbm_uint v = lbm_dec_as_u32(args[1]);
+		uint8_t reg = i2c_read_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG);
+		if (i <= 7) {
+			if (v == 0) {
+				reg &= ~(1 << i);
 			} else {
-				res = i2c_master_read_from_device(0, addr, read_buffer, read_size, 2000);
+				reg |= (1 << i);
+			}
+		}
+		i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG, (uint8_t)reg);
+		return ENC_SYM_TRUE;
+	}
+	return ENC_SYM_TERROR;
+}
+
+static lbm_value ext_read_button(lbm_value *args, lbm_uint argn) {
+	lbm_value res = ENC_SYM_TERROR;
+	if (argn == 1 && lbm_is_number(args[0])) {
+		int32_t button = lbm_dec_as_i32(args[0]);
+		button = button & 0x7;
+
+		//commands_printf_lisp("button = %d\n", button);
+
+		res = ENC_SYM_NIL;
+
+		if (button == 0) {
+			if (v_btn > 2.0) {
+				res = ENC_SYM_TRUE;
 			}
 		} else {
-			res = i2c_master_write_to_device(0, addr, write_buffer, write_size, 2000);
-		}
-
-		xSemaphoreGive(i2c_mutex);
-
-		return res;
-	}
-
-	static int i2c_read_reg(uint8_t addr, uint8_t reg) {
-		uint8_t tx_buf[1] = {reg};
-		uint8_t rx_buf[1];
-		esp_err_t e = i2c_tx_rx(addr, tx_buf, 1, rx_buf, 1);
-		if (e == ESP_OK) {
-			return rx_buf[0];
-		} else {
-			return -1;
-		}
-	}
-
-	static esp_err_t i2c_write_reg(uint8_t addr, uint8_t reg, uint8_t val) {
-		uint8_t tx_buf[2] = {reg, val};
-		return i2c_tx_rx(addr, tx_buf, 2, 0, 0);
-	}
-
-	void init_gpio_expander(void) {
-		i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_CONFIG_REG, 0x07); // Ports 0-2 Input
-		i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG, 0x00); // All Output Disable
-	}
-
-	static lbm_value ext_set_io(lbm_value *args, lbm_uint argn) {
-		if (argn == 2 && lbm_is_number(args[0]) && lbm_is_number(args[1])) {
-			lbm_uint i = lbm_dec_as_u32(args[0]);
-			lbm_uint v = lbm_dec_as_u32(args[1]);
-			uint8_t reg = i2c_read_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG);
-			if (i <= 7) {
-				if (v == 0) {
-					reg &= ~(1 << i);
-				} else {
-					reg |= (1 << i);
-				}
-			}
-			i2c_write_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_OUTPUT_REG, (uint8_t)reg);
-			return ENC_SYM_TRUE;
-		}
-		return ENC_SYM_TERROR;
-	}
-
-	static lbm_value ext_read_button(lbm_value *args, lbm_uint argn) {
-		lbm_value res = ENC_SYM_TERROR;
-		if (argn == 1 && lbm_is_number(args[0])) {
-			int32_t button = lbm_dec_as_i32(args[0]);
-			button = button & 0x7;
-
-			//commands_printf_lisp("button = %d\n", button);
-
-			res = ENC_SYM_NIL;
-
-			if (button == 0) {
-				if (v_btn > 2.0) {
-					res = ENC_SYM_TRUE;
-				}
-			} else {
-				uint8_t io = i2c_read_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_INPUT_REG);
-				//commands_printf_lisp("io = %x\n", io);
-				if (io & (1 << (button - 1))) {
-					res = ENC_SYM_TRUE;
-				}
+			uint8_t io = i2c_read_reg(I2C_ADDR_GPIO_EXP, GPIO_EXP_INPUT_REG);
+			//commands_printf_lisp("io = %x\n", io);
+			if (io & (1 << (button - 1))) {
+				res = ENC_SYM_TRUE;
 			}
 		}
-		return res;
 	}
+	return res;
+}
 #endif
 
 static lbm_value ext_hw_init(lbm_value *args, lbm_uint argn) {
