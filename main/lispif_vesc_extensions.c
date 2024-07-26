0b6b669b7b41acb0d5c6510bdaf0757e9679c666
Benjamin Vedder <benjamin@vedder.se>
2024-07-26 12:09:57 +0200

    Latest lbm

--- main/lispif_vesc_extensions.c
+++ main/lispif_vesc_extensions.c
@@ -5650,9 +5650,6 @@ void lispif_disable_all_events(void) {
 	}
 }
 
-// pull in from eval_cps
-void lbm_request_gc(void);
-
 static bool start_flatten_with_gc(lbm_flat_value_t *v, size_t buffer_size) {
 	if (lbm_start_flatten(v, buffer_size)) {
 		return true;
