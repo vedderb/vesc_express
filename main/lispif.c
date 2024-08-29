41d2351aa9f56ee2d84ba77c272c6e6040c4683a
Benjamin Vedder <benjamin@vedder.se>
2024-08-29 11:17:22 +0200

    Fixed typo

--- main/lispif.c
+++ main/lispif.c
@@ -145,7 +145,7 @@ static void print_ctx_info(eval_context_t *ctx, void *arg1, void *arg2) {
 	commands_printf_lisp("ContextID: %u", ctx->id);
 
 	if (ctx->name) {
-		commands_printf_lisp("Context Name: %u", ctx->name);
+		commands_printf_lisp("Context Name: %s", ctx->name);
 	}
 
 	commands_printf_lisp("Stack SP: %u",  ctx->K.sp);
