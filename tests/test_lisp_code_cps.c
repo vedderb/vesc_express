b8024840cc552349d6bf914ddbe0cdb106b01cf3
Benjamin Vedder <benjamin@vedder.se>
2025-01-21 11:59:27 +0100

    Squashed 'main/lispBM/' changes from b9fac251..4d944bc6
    4d944bc6 Updated loopwhile-thd dynamic function to allow specifying either or both the thread name and stack size
    5c24a6f0 remove forgotten debug print
    4dc679c7 loop macros moved into lbm_dyn_lib
    d574df58 tweak readme
    022eec32 bumping to 0.30.1
    166d32f7 version 0.30.0
    357b604d remove ref to matvec_extensions.h in lispbm.mk
    f46c619f removing matvec_extensions as it was mostly an example of custom types. Custom types will eventually be removed!
    83a7ca01 Static analysis is currently happy. Both Infer and scanbuild report 0 issues.
    c8b8c110 small fixes to test_lisp_code_cps.c
    e1197046 Added defstruct as an optionally dynamically loadable extension. Changed so that one OPTS-IN for dynload funs/macros rather than out using LBM_USE_DYN_FUNS,LBM_USE_DYN_MACROS. If you want defstruct use LBM_USE_DYN_DEFSTRUCT as well.
    7cc589f3 adding links to integration example gits
    dfef5693 adding building information to c docs
    7008f581 update readme
    f2514510 change to contribute section
    d1965f02 update copyright comment env.c
    66c6c714 update repl
    685dcd61 added a lbm_get_global_env_size function that gives the size of the env in number of heap cells
    3da936d1 update runtimeref.lisp/md
    1be58b55 added a way to turn on and off showing of error messages associated with trapped errors
    02ce4672 hiding the dyn_load strings behind a find function
    0c32bd71 added a defmacro macro. Changes to defstruct example in preparation to adding as an optional dyn-load
    d7670297 added defun and defunret based macro expander extensions and based defunret on unsafe cc
    
    git-subtree-dir: main/lispBM
    git-subtree-split: 4d944bc64acc4c6b0b742c96d78ca4d025153866

--- tests/test_lisp_code_cps.c
+++ tests/test_lisp_code_cps.c
@@ -29,11 +29,9 @@
 #include "extensions/math_extensions.h"
 #include "extensions/string_extensions.h"
 #include "extensions/runtime_extensions.h"
-#include "extensions/matvec_extensions.h"
 #include "extensions/random_extensions.h"
 #include "extensions/set_extensions.h"
 #include "extensions/mutex_extensions.h"
-#include "extensions/loop_extensions.h"
 #include "extensions/lbm_dyn_lib.h"
 #include "lbm_channel.h"
 #include "lbm_flat_value.h"
@@ -129,24 +127,6 @@ void critical_error(void) {
 
 // Dynamic loader
 
-static bool strmatch(const char *str1, const char *str2) {
-  size_t len = strlen(str1);
-
-  if (str2[len] != ' ') {
-    return false;
-  }
-
-  bool same = true;
-  for (unsigned int i = 0;i < len;i++) {
-    if (str1[i] != str2[i]) {
-      same = false;
-      break;
-    }
-  }
-
-  return same;
-}
-
 bool dyn_load(const char *str, const char **code) {
 
   size_t len = strlen(str);
@@ -194,28 +174,7 @@ bool dyn_load(const char *str, const char **code) {
     return true;
   }
 
-  for (unsigned int i = 0; i < (sizeof(loop_extensions_dyn_load) / sizeof(loop_extensions_dyn_load[0]));i++) {
-    if (strmatch(str, loop_extensions_dyn_load[i] + 8)) {
-      *code = loop_extensions_dyn_load[i];
-      return true;
-    }
-  }
-
-  for (unsigned int i = 0; i < (sizeof(lbm_dyn_macros) / sizeof(lbm_dyn_macros[0]));i++) {
-    if (strmatch(str, lbm_dyn_macros[i] + 8)) {
-      *code = lbm_dyn_macros[i];
-      return true;
-    }
-  }
-
-  for (unsigned int i = 0; i < (sizeof(lbm_dyn_fun) / sizeof(lbm_dyn_fun[0]));i++) {
-    if (strmatch(str, lbm_dyn_fun[i] + 7)) {
-      *code = lbm_dyn_fun[i];
-      return true;
-    }
-  }
-
-  return false;
+  return lbm_dyn_lib_find(str,code);
 }
 
   //lbm_value ext_even(lbm_value *args, lbm_uint argn) {
@@ -611,11 +570,10 @@ int main(int argc, char **argv) {
   lbm_math_extensions_init();
   lbm_string_extensions_init();
   lbm_runtime_extensions_init();
-  lbm_matvec_extensions_init();
   lbm_random_extensions_init();
   lbm_mutex_extensions_init();
   lbm_set_extensions_init();
-  lbm_loop_extensions_init();
+  lbm_dyn_lib_init();
 
   lbm_add_extension("ext-even", ext_even);
   lbm_add_extension("ext-odd", ext_odd);
@@ -650,7 +608,7 @@ int main(int argc, char **argv) {
 
   lbm_set_verbose(true);
 
-  printf("LBM memory free: %u words, %u bytes \n", lbm_memory_num_free(), lbm_memory_num_free() * sizeof(lbm_uint));
+  printf("LBM memory free: %"PRI_UINT" words, %"PRI_UINT" bytes \n", lbm_memory_num_free(), lbm_memory_num_free() * sizeof(lbm_uint));
 
   if (pthread_create(&lispbm_thd, NULL, eval_thd_wrapper, NULL)) {
     printf("Error creating evaluation thread\n");
@@ -693,7 +651,7 @@ int main(int argc, char **argv) {
   }
 
   test_cid = cid; // the result which is important for success or failure of test.
-  printf("test_cid = %d\n", test_cid);
+  printf("test_cid = %"PRI_INT"\n", test_cid);
 
   lbm_continue_eval();
   uint32_t stream_i = 0;
